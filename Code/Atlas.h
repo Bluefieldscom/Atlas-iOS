c9f10560a1b98ece574eb1c965f211f7ee6a763a
Neil B <neil477@gmail.com>
2015-11-05 11:19:38 -0800

    add mitoo atlas delegate to header file

--- Code/Atlas.h
+++ Code/Atlas.h
@@ -56,6 +56,7 @@
 #import "ATLMessagePresenting.h"
 #import "ATLParticipantPresenting.h"
 #import "ATLParticipant.h"
+#import "MitooConversationViewControllerDelegate.h"
 
 ///----------------
 /// @name Utilities
